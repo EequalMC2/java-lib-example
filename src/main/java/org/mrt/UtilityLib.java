ae6e8757e9d504ae3ffe1c5bfe0bf108119cc857
murat Oner <muratonr@yahoo.com.tr>
2024-04-13 20:25:59 +0300

    initial commit for java utilitylib

---
+++ src/main/java/org/mrt/UtilityLib.java
@@ -0,0 +1,13 @@
+package org.mrt;
+
+/**
+ * Hello world!
+ *
+ */
+public class UtilityLib 
+{
+    public void externalRepositoryTest( String[] args )
+    {
+        System.out.println( "External repository is able to be added the project!" );
+    }
+}
